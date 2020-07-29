0a5c7caa62fff693a91853075d593e79d85e1ceb
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2020-07-29 02:10:05 +0200

    added state dependant footer component

--- my-app/src/layout/Footer.js
+++ my-app/src/layout/Footer.js
@@ -1,9 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/footer.scss";
+
 const Footer = () => {
-  return (
-    <footer className="footer_styled">Created with fire and brimstone.</footer>
-  );
+  const [style, setStyle] = useState("one");
+  const handleClickCounter = () => {
+    if (style === "one") {
+      setStyle("two");
+    } else {
+      setStyle("one");
+    }
+  };
+
+  if (style === "one") {
+    return (
+      <>
+        <button
+          onClick={handleClickCounter}
+          style={{ width: "100px", height: "100px" }}
+        >
+          Style
+        </button>
+        <footer className="footer_styled">
+          Created with fire and brimstone.
+        </footer>
+      </>
+    );
+  } else if (style === "two") {
+    return (
+      <>
+        <button
+          onClick={handleClickCounter}
+          style={{ width: "100px", height: "100px" }}
+        >
+          Style
+        </button>
+        <footer className="footer_styled_two">
+          Created with fire and brimstone.
+        </footer>
+      </>
+    );
+  }
 };
 
 export default Footer;
--- my-app/src/layout/index.js
+++ my-app/src/layout/index.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
-const Layout = (props) => (
-  <div className="text-center p-10 flex-1"> {props.children}</div>
-);
+const Layout = (props) => <div> {props.children}</div>;
 export { Layout, Nav, Footer };
