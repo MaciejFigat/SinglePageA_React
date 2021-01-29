10661a0c064de6704daf0807a73cfb42e648bdc2
MaciejFigat <60787137+MaciejFigat@users.noreply.github.com>
2021-01-29 20:28:55 +0100

    cleanup 3.1

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Layout } from '../layout'
 
 import { motion } from 'framer-motion'
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Home from './Home'
 import About from './About'
 import Contact from './Contact'
